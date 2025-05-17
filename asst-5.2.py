1430e19abecdc421110437ad1979c0816ea54b4a
Arshad Shaik <mail2arshad113@gmail.com>
2025-05-17 21:23:06 +0530

    Updated asst-5.2.py

--- asst-5.2.py
+++ asst-5.2.py
@@ -1,6 +1,6 @@
 l = [i for i in range(1,11)]
 print("Original list: ", l)
 l1 = l[:5]
-rev = l1[::-1]
 print("Extracted first 5 elements: ",l1)
-print("Reversed extracted elements: ",rev)
+l1.reverse()
+print("Reversed extracted elements: ",l1)
